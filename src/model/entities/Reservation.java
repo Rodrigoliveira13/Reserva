c5078689ee6fc0faf909b031b6341dedbf04117a
rodrigooliveira <rodrigo.oliveirags14@gmail.com>
2021-09-09 18:30:41 -0300

    Solução menos pior

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -41,9 +41,17 @@ public class Reservation {
         long diff = checkOut.getTime() - chekIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    public void updateDates(Date chekIn, Date  checkOut){
+    public String updateDates(Date checkIn, Date  checkOut){
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)){
+           return  "Error in reservation: Reservation dater for updates must be future";
+        }
+        if (!checkOut.after(checkIn)){
+            return  "Error in reservation: Check out date must be after check in date";
+        }
         this.chekIn = chekIn;
         this.checkOut = checkOut;
+        return null;
     }
     @Override
     public String toString(){
